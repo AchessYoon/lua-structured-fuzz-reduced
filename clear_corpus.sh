e87130b712f5557081b77b45268192ea4822d519
yoon_surface_wsl <hsy6119@naver.com>
2023-06-09 16:57:39 +0900

    improve initial seed

--- clear_corpus.sh
+++ clear_corpus.sh
@@ -2,5 +2,6 @@
 
 rm -r corpus/fuzz_llex
 mkdir corpus/fuzz_llex
-touch corpus/fuzz_llex/1
-chmod 775 corpus/fuzz_llex/1
+cp ./initial_seed.lua corpus/fuzz_llex/1
+# touch corpus/fuzz_llex/1
+# chmod 775 corpus/fuzz_llex/1
