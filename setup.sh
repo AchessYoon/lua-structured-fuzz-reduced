ec392ad6d4c08a34a07a180254fdaa45706ca6b4
yoon_surface_wsl <hsy6119@naver.com>
2023-06-17 17:18:15 +0900

    setup including metalua

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ rm -rf build.fuzz
 mkdir build.fuzz && cd build.fuzz
 cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 cd ..
+
+cp -r metalua.setup/metalua build.fuzz/metalua
+cp metalua.setup/metalua.lua build.fuzz/metalua.lua
+cp metalua.setup/checks.lua build.fuzz/checks.lua
