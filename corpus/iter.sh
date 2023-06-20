756735219021abbce57b5bd2d3ab6bc4e8f037a4
Jiseok Kim <wltjr1002@gmail.com>
2023-06-20 16:35:35 +0900

    improve Diff tester

--- corpus/iter.sh
+++ corpus/iter.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-cnt=$(ls ./fuzz_llex | wc -l)
+target_dir="./fuzz_lparse_result"
+
+lua_output_path="./result/lua_out.txt"
+luajit_output_path="./result/luajit_out.txt"
+gopherlua_output_path="./result/gopher_out.txt"
+lua2c_output_path="./result/lua2c_out.txt"
+
+cnt=$(ls $target_dir | wc -l)
 echo $cnt files
 
-echo "" > out.txt
-for filename in ./result/fuzz_llex_0610_001/* ; do
-    # result=$(echo "$filename")
+rm -rf ./result
+mkdir result
 
-    # ./itersample.sh
+# echo "" > $lua_output_path
+# echo "" > $luajit_output_path
+# echo "" > $gopherlua_output_path
+echo "" > $lua2c_output_path
+for filename in $target_dir/* ; do
+    result=$(echo "$filename")
+
     # lua $filename
+    # result=$?
+    # sudo echo "$filename, $result" >> $lua_output_path
+
     # luajit -b $filename luajit.out
-    # env GOPHER_TARGET=$filename GOPATH="/home/hsy6119/go" go run run_lua.go
-    ./lua2c/clua -c $filename
+    # result=$?
+    # sudo echo "$filename, $result" >> $luajit_output_path
+
+    # env GOPHER_TARGET=$filename go run run_lua.go
+    # result=$?
+    # sudo echo "$filename, $result" >> $gopherlua_output_path
+    
+    lua ./lua2c/lua2c.lua -c $filename
     result=$?
+    sudo echo $result >> $lua2c_output_path
 
-    # stdout=$( ./itersample.sh )
-    # result=$( (echo $stdout | grep -Eq  ^.*success.*$) && echo 0 || echo 1 )
+    #stdout=$( ./itersample.sh )
+    #result=$( (echo $stdout | grep -Eq  ^.*success.*$) && echo 0 || echo 1 )
 
-    sudo echo $result >> out.txt
 
     # stat -c '%y' $filename
 done
